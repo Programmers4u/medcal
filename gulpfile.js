9a473fff5c5fa33ec815e9a539e8d4034b6e9498
Marcin Kania <job@gatesms.eu>
2019-12-27 13:51:20 +0100

    fix

--- gulpfile.js
+++ gulpfile.js
@@ -2,59 +2,10 @@ var gulp  = require('gulp');
 var cleanCSS = require('gulp-clean-css');
 var rename = require("gulp-rename");
 var uglify = require('gulp-uglify');
-//var pkg = require('./package.json');
-//var browserSync = require('browser-sync').create();
 var less = require('gulp-less');
 var path = require('path');
 var util = require("gulp-util");
-//var watch = require('gulp-watch');
-
-// Copy third party libraries from /node_modules into /vendor
-gulp.task('vendor', function() {
-
-  // Bootstrap
-  gulp.src([
-      './node_modules/bootstrap/dist/**/*',
-      '!./node_modules/bootstrap/dist/css/bootstrap-grid*',
-      '!./node_modules/bootstrap/dist/css/bootstrap-reboot*'
-    ])
-    .pipe(gulp.dest('./assets/vendor/bootstrap'))
-
-  // Font Awesome
-  gulp.src([
-      './node_modules/font-awesome/**/*',
-      '!./node_modules/font-awesome/{less,less/*}',
-      '!./node_modules/font-awesome/{scss,scss/*}',
-      '!./node_modules/font-awesome/.*',
-      '!./node_modules/font-awesome/*.{txt,json,md}'
-    ])
-    .pipe(gulp.dest('./assets/vendor/font-awesome'))
-
-  // jQuery
-  gulp.src([
-      './node_modules/jquery/dist/*',
-      '!./node_modules/jquery/dist/core.js'
-    ])
-    .pipe(gulp.dest('./assets/vendor/jquery'))
-
-  // jQuery Easing
-  gulp.src([
-      './node_modules/jquery.easing/*.js'
-    ])
-    .pipe(gulp.dest('./assets/vendor/jquery-easing'))
-
-  // Simple Line Icons
-  gulp.src([
-      './node_modules/simple-line-icons/fonts/**',
-    ])
-    .pipe(gulp.dest('./assets/vendor/simple-line-icons/fonts'))
-
-  gulp.src([
-      './node_modules/simple-line-icons/css/**',
-    ])
-    .pipe(gulp.dest('./assets/vendor/simple-line-icons/css'))
-
-});
+var elixir = require('laravel-elixir');
 
 // Compile LESS
 gulp.task('less', function () {
@@ -95,7 +46,6 @@ gulp.task('js:minify', function() {
       suffix: '.min'
     }))
     .pipe(gulp.dest('./public/assets/js'));
-    //.pipe(browserSync.stream());
 });
  
 // Dev task
@@ -103,5 +53,136 @@ gulp.task('dev', function() {
   util.log('Dev start');
   gulp.watch('./resources/assets/js/*.js', gulp.parallel('js:minify'));
   gulp.watch('./resources/assets/less/*.less', gulp.series( 'less','css:minify' ));
-  //gulp.watch('./*.html', browserSync.reload);
+});
+
+/*
+ |--------------------------------------------------------------------------
+ | Elixir Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Elixir provides a clean, fluent API for defining some basic Gulp tasks
+ | for your Laravel application. By default, we are compiling the Less
+ | file for our application, as well as publishing vendor resources.
+ |
+ */
+
+elixir(function(mix) {
+
+    mix.copy([
+        './bower_components/adminlte/plugins/iCheck/square/*.png',
+    ], 'public/css/iCheck/');
+
+    mix.styles([
+        './bower_components/adminlte/plugins/iCheck/square/blue.css'
+    ], 'public/css/iCheck/icheck.min.css');
+
+    mix.scripts([
+        './bower_components/adminlte/plugins/iCheck/icheck.min.js',
+    ], 'public/js/iCheck/icheck.min.js');
+
+    mix.scripts([
+        './bower_components/clipboard/dist/clipboard.min.js',
+    ], 'public/js/clipboard/clipboard.min.js');
+
+    mix.styles([
+        './bower_components/bootstrap/dist/css/bootstrap.min.css',
+        './bower_components/adminlte/dist/css/AdminLTE.css',
+        './bower_components/adminlte/dist/css/skins/skin-blue.css',
+        './bower_components/adminlte/plugins/iCheck/square/blue.css'
+    ], 'public/css/app.min.css');
+
+    mix.scripts([
+        './bower_components/jquery/dist/jquery.min.js',
+        './bower_components/moment/min/moment-with-locales.min.js',
+        './bower_components/moment/min/locales.min.js',
+        './bower_components/moment-timezone/builds/moment-timezone.min.js',
+        './bower_components/adminlte/plugins/iCheck/icheck.min.js',
+        './bower_components/bootstrap/dist/js/bootstrap.min.js',
+        './bower_components/adminlte/dist/js/app.min.js',
+        './bower_components/tooltipster/js/jquery.tooltipster.min.js'
+    ], 'public/js/app.min.js');
+
+    mix.styles([
+        // './bower_components/bootstrap/dist/css/bootstrap.min.css',
+        './bower_components/tooltipster/css/themes/tooltipster-light.css',
+        './bower_components/tooltipster/css/tooltipster.css',
+        './bower_components/animate.css/animate.min.css',
+    ], 'public/css/styles.css');
+
+    // News Helpers
+
+    mix.scripts([
+        './bower_components/jquery-bootstrap-newsbox/dist/jquery.bootstrap.newsbox.min.js',
+    ], 'public/js/newsbox.js');
+
+    // Form Helpers
+
+    mix.scripts([
+        './bower_components/select2/dist/js/select2.full.min.js',
+        './bower_components/bootstrap-validator/dist/validator.min.js',
+        './bower_components/speakingurl/speakingurl.min.js',
+        './bower_components/jquery-slugify/dist/slugify.min.js',
+        './bower_components/bootstrap-list-filter/bootstrap-list-filter.min.js',
+        './bower_components/mjolnic-bootstrap-colorpicker/dist/js/bootstrap-colorpicker.min.js',
+        './bower_components/jquery.steps/build/jquery.steps.min.js',
+        './bower_components/bootstrap-switch/dist/js/bootstrap-switch.min.js',
+    ], 'public/js/forms.js');
+
+    mix.copy([
+        './bower_components/select2/dist/js/i18n/*.min.js',
+    ], 'public/js/select2/i18n');
+
+    mix.copy([
+        './bower_components/mjolnic-bootstrap-colorpicker/dist/img/',
+    ], 'public/img/');
+
+    mix.styles([
+        './bower_components/select2/dist/css/select2.min.css',
+        './bower_components/select2-bootstrap-theme/dist/select2-bootstrap.min.css',
+        './bower_components/mjolnic-bootstrap-colorpicker/dist/css/bootstrap-colorpicker.min.css',
+        './bower_components/jquery.steps/demo/css/jquery.steps.css',
+        './bower_components/bootstrap-switch/dist/css/bootstrap3/bootstrap-switch.min.css'
+    ], 'public/css/forms.css');
+
+    // Highlight
+
+    mix.scripts([
+        './bower_components/jquery-highlighttextarea/jquery.highlighttextarea.min.js',
+    ], 'public/js/highlight.js');
+
+    mix.styles([
+        './bower_components/jquery-highlighttextarea/jquery.highlighttextarea.min.css',
+    ], 'public/css/highlight.css');
+
+    // Date & Time Helpers
+
+    mix.scripts([
+        // './bower_components/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js',
+        './bower_components/air-datepicker/dist/js/datepicker.min.js',
+        './bower_components/air-datepicker/dist/js/i18n/datepicker.es.js',
+        './bower_components/air-datepicker/dist/js/i18n/datepicker.en.js',
+        //'./bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js',
+        './bower_components/bootstrap-timepicker/js/bootstrap-timepicker.js',
+        './bower_components/fullcalendar/dist/fullcalendar.min.js',
+        './bower_components/fullcalendar/dist/locale-all.js',
+    ], 'public/js/datetime.js');
+
+    mix.styles([
+        // './bower_components/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.min.css',
+        './bower_components/air-datepicker/dist/css/datepicker.min.css',
+        //'./bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css',
+        './bower_components/bootstrap-timepicker/css/timepicker.less',
+        './bower_components/fullcalendar/dist/fullcalendar.min.css',
+    ], 'public/css/datetime.css');
+
+    // Tour Helpers
+
+    mix.scripts([
+        './bower_components/bootstrap-tour/build/js/bootstrap-tour.min.js',
+    ], 'public/js/tour.js');
+
+    mix.styles([
+        './bower_components/bootstrap-tour/build/css/bootstrap-tour.min.css',
+    ], 'public/css/tour.css');
+
 });
