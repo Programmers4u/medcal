20835aea36e5b05a0543c83a183eb23c806bd529
Marcin Kania <job@gatesms.eu>
2020-01-28 13:32:01 +0100

    fix

--- resources/views/_user-search-menu.blade.php
+++ resources/views/_user-search-menu.blade.php
@@ -1,4 +1,4 @@
-<li>
+<li id='search-bar-menu'>
     {!! Form::open(['method' => 'post', 'url' => route('manager.search', $business), 'class' => 'navbar-form navbar-left', 'role' => 'search']) !!}
     <div class="form-group">
         <input id="search" style="border-radius: 4px;" name="criteria" type="text" class="form-control" placeholder="{{trans('app.search.placeholder')}}">
@@ -8,4 +8,15 @@
         </span>
     </div>
     {!! Form::close() !!}
-</li>  
+</li>
+
+<style>
+    @media all and (min-width: 500px) {
+        #search-bar-menu {
+            display: block;
+        };
+    };
+    #search-bar-menu {
+        display: none !important;
+    };
+</style>
