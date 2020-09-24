a8d3f5851e27fc8f4a6bf14afae305826dd0fef2
Marcin Kania <marcin@programmers4u.com>
2020-09-24 12:39:43 +0200

    init confirm

--- resources/assets/js/confirm/confirm.js
+++ resources/assets/js/confirm/confirm.js
@@ -1,10 +1,24 @@
-var confirm = function(message){
+var confirm = function(message, resultFunc) {
     console.log('CONFIRM:',message);
     $('#confirm_info_success').text(message);
     $('#confirm_info_success').toggle();
-    setTimeout(function(){$('#confirm_info_success').toggle(400);$('#confirmModal button').click();},1500);
+    // setTimeout(function(){$('#confirm_info_success').toggle(400);$('#confirmModal button').click();},1500);
     $('#confirmModal').modal({
         keyboard: false,
         backdrop: false,
-    })    
+    });
+    $("#confirmYes").on("click", function () {
+        $('#confirm_info_success').toggle(400);
+        $('#confirmModal button').click(); 
+        if(resultFunc)
+            resultFunc(true);
+        // return true;
+    });    
+    $("#confirmNo").on("click", function () {
+        $('#confirm_info_success').toggle(400);
+        $('#confirmModal button').click(); 
+        if(resultFunc)
+            resultFunc(false);
+        // return false;
+    });    
 }
