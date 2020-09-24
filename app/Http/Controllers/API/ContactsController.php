56c367fd175f53d15f5e5ba997f92f43be3a7a25
Marcin Kania <marcin@programmers4u.com>
2020-09-24 04:21:51 +0200

    add job to process file import

--- app/Http/Controllers/API/ContactsController.php
+++ app/Http/Controllers/API/ContactsController.php
@@ -8,6 +8,7 @@ use Timegridio\Concierge\Models\Business;
 use \Carbon\Carbon;
 use App\Http\Requests\Statistics\GetRequest;
 use App\Http\Resources\Statistics\DefaultResources;
+use App\Jobs\ProcessContactImport;
 use App\Models\Datasets;
 use App\Models\MedicalHistory;
 use Illuminate\Http\JsonResponse;
@@ -21,40 +22,11 @@ class ContactsController extends Controller
     public function importFromFile(GetRequest $request, Business $business) : JsonResponse
     {
         $response = ['status'=>'ok','data'=>'','error'=>''];
-
         $uploadedFile = $request->file()[0];
-        // $filename = $uploadedFile->getClientOriginalName();
         $file = Storage::disk('local')->putFile('temp', $uploadedFile);
-        // dd($file);
         $contacts = explode("\n" ,Storage::get($file));
         $response['data'] = 'Ilość rekordów: ' . count($contacts);
-        // if (count($contacts) > plan('limits.contacts', $business->plan)) {
-        //     $response['error'] = trans('app.saas.plan_limit_reached');
-        //     $response['status'] = 'error';
-        // };
-
-        // dd($contacts);
-    foreach($contacts as $index => $contact) {
-        // dd($contact);
-        if($index > 200) break;
-            // if($business->contacts()->count() > plan('limits.contacts', $business->plan))
-                // break;
-            $register = [
-                'firstname' => $contact[1],
-                'lastname' => $contact[2],
-                'nin' => $contact[3],
-                'gender' => $contact[4],
-                'birthdate' => Carbon::parse($contact[5]),
-                'mobile' => $contact[6],
-                'email' => $contact[7],
-                // 'mobile_country' => $contact['mobile_country']
-            ];
-            
-            $business->addressbook()->register($register);
-        };
-        
-        Storage::disk('local')->delete($file);
-
+        dispatch(new ProcessContactImport($file, $business));
         return response()->json($response);
     }
 }
