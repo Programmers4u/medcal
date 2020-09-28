fa1e788f0ea2e33761706acd6a6c2009baf24f63
Marcin Kania <marcin@programmers4u.com>
2020-09-28 20:55:09 +0200

    fix confirm

--- resources/views/manager/contacts/show.blade.php
+++ resources/views/manager/contacts/show.blade.php
@@ -173,7 +173,7 @@
                 
             {!! Button::danger()
                 ->withIcon(Icon::trash())
-                ->asLinkTo( route('manager.addressbook.destroy', [$business, $contact]) )
+                ->asLinkTo( "javascript:remove()" )
                 ->withAttributes([
                     'id' => 'delete-btn',
                     'type' => 'button',
@@ -336,78 +336,85 @@ $(document).ready(function() {
     
 });
 
-(function() {
- 
-    var laravel = {
-        initialize: function() {
-            this.methodLinks = $('a[data-method]');
- 
-            this.registerEvents();
-        },
- 
-        registerEvents: function() {
-            this.methodLinks.on('click', this.handleMethod);
-        },
- 
-        handleMethod: function(e) {
-            var link = $(this);
-            var httpMethod = link.data('method').toUpperCase();
-            var form;
- 
-            // If the data-method attribute is not PUT or DELETE,
-            // then we don't know what to do. Just ignore.
-            if ( $.inArray(httpMethod, ['PUT', 'DELETE']) === - 1 ) {
-                return;
-            }
- 
-            // Allow user to optionally provide data-confirm="Are you sure?"
-            if ( link.data('confirm') ) {
-                if ( ! laravel.verifyConfirm(link) ) {
-                    return false;
-                }
-            }
- 
-            form = laravel.createForm(link);
-            form.submit();
- 
-            e.preventDefault();
-        },
- 
-        verifyConfirm: function(link) {
-            return confirm(link.data('confirm'));
-        },
- 
-        createForm: function(link) {
-            var form =
-            $('<form>', {
-                'method': 'POST',
-                'action': link.attr('href')
-            });
- 
-            var token =
-            $('<input>', {
-                'type': 'hidden',
-                'name': '_token',
-                'value': '{{ csrf_token() }}'
-                });
- 
-            var hiddenInput =
-            $('<input>', {
-                'name': '_method',
-                'type': 'hidden',
-                'value': link.data('method')
-            });
- 
-            return form.append(token, hiddenInput).appendTo('body');
-        }
-    };
- 
-    laravel.initialize();
+var remove = () => {
+    
+    removeService("{{ trans('manager.contacts.btn.confirm_delete').'?' }}", "{{ route('manager.addressbook.destroy', [$business, $contact]) }}");
+}
 
-    $("img").error(function(){
-        $(this).hide();
-    });
+// (function() {
+ 
+//     var laravel = {
+//         initialize: function() {
+//             this.methodLinks = $('a[data-method]');
+ 
+//             this.registerEvents();
+//         },
+ 
+//         registerEvents: function() {
+//             this.methodLinks.on('click', this.handleMethod);
+//         },
+ 
+//         handleMethod: function(e) {
+//             var link = $(this);
+//             var httpMethod = link.data('method').toUpperCase();
+//             var form;
+ 
+//             // If the data-method attribute is not PUT or DELETE,
+//             // then we don't know what to do. Just ignore.
+//             if ( $.inArray(httpMethod, ['PUT', 'DELETE']) === - 1 ) {
+//                 return;
+//             }
+ 
+//             // Allow user to optionally provide data-confirm="Are you sure?"
+//             if ( link.data('confirm') ) {
+//                 if ( ! laravel.verifyConfirm(link) ) {
+//                     return false;
+//                 }
+//             }
+ 
+//             form = laravel.createForm(link);
+//             form.submit();
+ 
+//             e.preventDefault();
+//         },
+ 
+//         verifyConfirm: function(link) {
+//             return confirm(link.data('confirm'));
+//         },
+ 
+//         createForm: function(link) {
+//             var form =
+//             $('<form>', {
+//                 'method': 'POST',
+//                 'action': link.attr('href')
+//             });
+ 
+//             var token =
+//             $('<input>', {
+//                 'type': 'hidden',
+//                 'name': '_token',
+//                 'value': '{{ csrf_token() }}'
+//                 });
+ 
+//             var hiddenInput =
+//             $('<input>', {
+//                 'name': '_method',
+//                 'type': 'hidden',
+//                 'value': link.data('method')
+//             });
+ 
+//             return form.append(token, hiddenInput).appendTo('body');
+//         }
+//     };
+ 
+//     laravel.initialize();
 
-})();
+//     $("img").error(function(){
+//         $(this).hide();
+//     });
+
+// })();
+
 </script>
+<script type="text/javascript" src="/js/service/services.min.js"></script>
 @endpush
