5c405da83b824d6257376ddfc34e383b80fb35d4
Marcin Kania <marcin@programmers4u.com>
2020-10-24 21:39:16 +0200

    init to client

--- app/Listeners/SendMedicalHistoryPdfNotification.php
+++ app/Listeners/SendMedicalHistoryPdfNotification.php
@@ -23,37 +23,34 @@ class SendMedicalHistoryPdfNotification implements ShouldQueue
 
     public function handle(SendMedicalHistoryPdf $event)
     {
-        // $this->sendEmailToContact($event);
+        $this->sendEmailToContact($event);
         $this->sendEmailToOwner($event);
     }
     
     protected function sendEmailToContact(Event $event)
     {
 
-        // if (!$user = $event->user) {
-        //     // logger()->debug('not user');
-        //     return false;
-        // }
+        $destinationEmail = $event->contact->email;
+        if(empty($destinationEmail)) return;
 
-        // $params = [
-        //     'user'        => $user,
-        //     'appointment' => $event->appointment,
-        //     'userName'    => $event->appointment->contact->firstname,
-        //     'businessName' => $event->appointment->business->name,
-        // ];
-        // $header = [
-        //     'name'  => $event->appointment->contact->firstname,
-        //     'email' => $destinationEmail,
-        // ];
-        // $email = [
-        //     'header'   => $header,
-        //     'params'   => $params,
-        //     'locale'   => $event->appointment->business->locale,
-        //     'timezone' => $user->pref('timezone'),
-        //     'template' => 'user.appointment-notification.notification',
-        //     'subject'  => 'user.appointment-notification.subject',
-        // ];
-        // $this->sendemail($email);
+        $params = [
+            'user'        => $event->contact->firstname,
+            'ownerName'    => $event->contact->firstname,
+        ];
+        $header = [
+            'name'  => $event->contact->firstname,
+            'email' => $destinationEmail,
+        ];
+        $email = [
+            'filePathName' => [$event->pathFile],
+            'header'   => $header,
+            'params'   => $params,
+            'locale'   => $event->business->locale,
+            'timezone' => $event->business->owner()->pref('timezone'),
+            'template' => 'manager.medical-history-notification.notification',
+            'subject'  => 'manager.medical-document-notification.subject',
+        ];
+        $this->sendemail($email);
     }
 
     protected function sendEmailToOwner($event)
