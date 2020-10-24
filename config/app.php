eb803e0d623dca614c6599205103552ef524a061
Marcin Kania <marcin@programmers4u.com>
2020-10-24 17:57:14 +0200

    wip

--- config/app.php
+++ config/app.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-
+    // 'Arr' => Illuminate\Support\Arr::class,
     /*
     |--------------------------------------------------------------------------
     | Application Environment
