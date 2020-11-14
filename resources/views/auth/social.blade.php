3332a523f51f7550d9a4df08485637e8eeae43a2
Marcin Kania <marcin@programmers4u.com>
2020-11-14 02:02:24 +0100

    fix logo google

--- resources/views/auth/social.blade.php
+++ resources/views/auth/social.blade.php
@@ -17,7 +17,7 @@
 <div class="row">
     <div class="col-md-12 text-center">
         <a class="btn btn-block btn-social btn-facebook btn-flat" href="{{ route('social.login', ['facebook']) }}"><i class="fa fa-facebook"></i> {{ trans('auth.social.facebook') }}</a>
-        <a class="btn btn-block btn-social btn-google btn-flat" href="{{ route('social.login', ['google']) }}"><i class="fa fa-google-plus"></i> {{ trans('auth.social.google') }}</a>
+        <a class="btn btn-block btn-social btn-google btn-flat" href="{{ route('social.login', ['google']) }}"><i class="fa fa-google"></i> {{ trans('auth.social.google') }}</a>
         {{-- <a class="btn btn-block btn-social btn-github btn-flat" href="{{ route('social.login', ['github']) }}"><i class="fa fa-github"></i> {{ trans('auth.social.github') }}</a> --}}
         <a class="btn btn-block btn-social btn-linkedin btn-flat" href="{{ route('social.login', ['linkedin']) }}"><i class="fa fa-linkedin"></i> {{ trans('auth.social.linkedin') }}</a>
     </div>
