89b9d3d9dae763ff9f31286fd53a929fe94bbea7
Marcin Kania <marcin@programmers4u.com>
2020-11-16 01:06:10 +0100

    init laravel 5.6

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,12 @@
 
 namespace App\Providers;
 
+use Illuminate\Contracts\Pagination\Paginator as PaginationPaginator;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Support\Facades\Blade;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,7 +26,6 @@ class AppServiceProvider extends ServiceProvider
         if ($this->app->environment() == 'local') {
             $this->app->register(\Laracasts\Generators\GeneratorsServiceProvider::class);
             $this->app->register(\Barryvdh\Debugbar\ServiceProvider::class);
-            // $this->app->register(\Potsky\LaravelLocalizationHelpers\LaravelLocalizationHelpersServiceProvider::class);
         }
 
         if (config('services.rollbar.access_token', false)) {
@@ -41,5 +42,16 @@ class AppServiceProvider extends ServiceProvider
                         //->withPath('');
                 });
         }        
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Blade::withoutDoubleEncoding();
+        Paginator::useBootstrapThree();
     }
 }
