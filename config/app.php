3149ce5ac7d4c4253654396ef532a17df4304464
Marcin Kania <marcin@programmers4u.com>
2020-11-16 17:37:00 +0100

    fix log and arr

--- config/app.php
+++ config/app.php
@@ -1,7 +1,6 @@
 <?php
 
 return [
-    // 'Arr' => Illuminate\Support\Arr::class,
     /*
     |--------------------------------------------------------------------------
     | Application Environment
@@ -94,22 +93,6 @@ return [
     'key' => env('APP_KEY'),
 
     'cipher' => 'AES-256-CBC',
-
-    /*
-    |--------------------------------------------------------------------------
-    | Logging Configuration
-    |--------------------------------------------------------------------------
-    |
-    | Here you may configure the log settings for your application. Out of
-    | the box, Laravel uses the Monolog PHP logging library. This gives
-    | you a variety of powerful log handlers / formatters to utilize.
-    |
-    | Available Settings: "single", "daily", "syslog", "errorlog"
-    |
-    */
-
-    // 'log' => env('APP_LOG', 'daily'),
-    // 'log_level' => env('APP_LOG_LEVEL', 'error'),
 
     /*
     |--------------------------------------------------------------------------
